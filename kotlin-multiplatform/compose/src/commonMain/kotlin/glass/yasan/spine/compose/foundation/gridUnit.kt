6209df46ad17bcb3d1e2be596b8a2f9fcd6dbfa0
Yasan Glass <yasanglass@gmail.com>
2025-07-20 18:02:15 +0200

    Import older versions of `grid` from Spine

--- kotlin-multiplatform/compose/src/commonMain/kotlin/glass/yasan/spine/compose/foundation/gridUnit.kt
+++ kotlin-multiplatform/compose/src/commonMain/kotlin/glass/yasan/spine/compose/foundation/gridUnit.kt
@@ -1,5 +1,6 @@
 package glass.yasan.spine.compose.foundation
 
+import androidx.annotation.IntRange
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -19,3 +20,17 @@ public inline val Int.grid: Dp get() = gridUnit * this
     ReplaceWith("(this * 8).dp", "androidx.compose.ui.unit.dp")
 )
 public inline val Float.grid: Dp get() = gridUnit * this
+
+@Stable
+@Deprecated(
+    message = "Use dp value instead",
+    ReplaceWith("(multiplier * 8).dp", "androidx.compose.ui.unit.dp")
+)
+public fun grid(multiplier: Float): Dp = gridUnit * multiplier
+
+@Stable
+@Deprecated(
+    message = "Use dp value instead",
+    ReplaceWith("(multiplier * 8).dp", "androidx.compose.ui.unit.dp")
+)
+public fun grid(@IntRange(from = 2) multiplier: Int): Dp = gridUnit * multiplier
