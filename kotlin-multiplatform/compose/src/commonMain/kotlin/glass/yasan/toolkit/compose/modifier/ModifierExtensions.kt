d9d0b0883e2ea2f37607feab229cc76d4fef53b6
Yasan Glass <yasanglass@gmail.com>
2025-08-03 00:23:12 +0200

    Fix Detekt issues

--- kotlin-multiplatform/compose/src/commonMain/kotlin/glass/yasan/toolkit/compose/modifier/ModifierExtensions.kt
+++ kotlin-multiplatform/compose/src/commonMain/kotlin/glass/yasan/toolkit/compose/modifier/ModifierExtensions.kt
@@ -2,6 +2,16 @@ package glass.yasan.toolkit.compose.modifier
 
 import androidx.compose.ui.Modifier
 
-public inline fun Modifier.addIf(condition: Boolean, crossinline factory: Modifier.() -> Modifier): Modifier = if (condition) factory() else this
+public inline fun Modifier.addIf(condition: Boolean, crossinline factory: Modifier.() -> Modifier): Modifier =
+    if (condition) {
+        factory()
+    } else {
+        this
+    }
 
-public inline fun <T> Modifier.addIfNotNull(value: T?, crossinline factory: Modifier.(T) -> Modifier): Modifier = if (value != null) factory(value) else this
+public inline fun <T> Modifier.addIfNotNull(value: T?, crossinline factory: Modifier.(T) -> Modifier): Modifier =
+    if (value != null) {
+        factory(value)
+    } else {
+        this
+    }
