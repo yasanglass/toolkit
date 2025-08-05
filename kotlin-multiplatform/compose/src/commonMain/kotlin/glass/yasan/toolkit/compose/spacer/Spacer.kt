1816d7f31616622c392377b6f906c6800cfed64d
Yasan Glass <yasanglass@gmail.com>
2025-08-05 23:25:11 +0200

    Create Spacer.kt

---
+++ kotlin-multiplatform/compose/src/commonMain/kotlin/glass/yasan/toolkit/compose/spacer/Spacer.kt
@@ -0,0 +1,72 @@
+package glass.yasan.toolkit.compose.spacer
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+public fun Spacer(
+    height: Dp,
+    width: Dp,
+) {
+    Spacer(
+        modifier = Modifier
+            .height(height)
+            .width(width),
+    )
+}
+
+@Composable
+public fun VerticalSpacer(
+    height: Dp,
+) {
+    Spacer(
+        height = height,
+        width = 0.dp,
+    )
+}
+
+@Composable
+public fun HorizontalSpacer(
+    width: Dp,
+) {
+    Spacer(
+        height = 0.dp,
+        width = width,
+    )
+}
+
+public fun LazyListScope.spacer(
+    height: Dp,
+    width: Dp,
+) {
+    item {
+        Spacer(
+            height = height,
+            width = width,
+        )
+    }
+}
+
+public fun LazyListScope.horizontalSpacer(
+    width: Dp,
+) {
+    spacer(
+        height = 0.dp,
+        width = width,
+    )
+}
+
+public fun LazyListScope.verticalSpacer(
+    height: Dp,
+) {
+    spacer(
+        height = height,
+        width = 0.dp,
+    )
+}
