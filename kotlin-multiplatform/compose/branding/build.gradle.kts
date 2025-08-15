712811014c772cc7a06b11f1113920708eeef92f
Yasan Glass <yasanglass@gmail.com>
2025-08-16 00:13:25 +0200

    Create compose.branding module

---
+++ kotlin-multiplatform/compose/branding/build.gradle.kts
@@ -0,0 +1,70 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+val artifactId = "compose-branding"
+
+plugins {
+    alias(libs.plugins.jetbrains.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.jetbrains.kotlin.compose)
+}
+
+kotlin {
+    explicitApi()
+
+    androidTarget {
+        publishLibraryVariants("release")
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+    jvm()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    applyDefaultHierarchyTemplate()
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(project(":compose"))
+
+                implementation(compose.components.resources)
+                implementation(compose.foundation)
+                implementation(compose.material3)
+                implementation(compose.materialIconsExtended)
+                implementation(compose.runtime)
+                implementation(compose.ui)
+
+                implementation(libs.coil.compose)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "glass.yasan.toolkit.compose.branding"
+    compileSdk = libs.versions.android.sdk.compile.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.sdk.min.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+compose.resources {
+    publicResClass = false
+    packageOfResClass = "glass.yasan.toolkit.compose.branding"
+    generateResClass = auto
+}
+
+configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
+    coordinates(artifactId = artifactId)
+}
