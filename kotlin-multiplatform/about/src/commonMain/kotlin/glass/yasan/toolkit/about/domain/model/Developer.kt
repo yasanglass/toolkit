391cc1ebb8db13b8fdbe6701400cdbf907df850a
Yasan Glass <yasanglass@gmail.com>
2025-10-02 00:36:01 +0200

    Add more social links

--- kotlin-multiplatform/about/src/commonMain/kotlin/glass/yasan/toolkit/about/domain/model/Developer.kt
+++ kotlin-multiplatform/about/src/commonMain/kotlin/glass/yasan/toolkit/about/domain/model/Developer.kt
@@ -8,12 +8,14 @@ public data class Developer(
 
     public data class Socials(
         val bluesky: String = "https://bsky.app/profile/yasan.glass",
+        val discordServer: String = "https://discord.gg/8BQrfyA",
         val email: String = "mailto:yasanglass@gmail.com",
         val github: String = "https://github.com/yasanglass",
         val gumroad: String = "https://yasanglass.gumroad.com",
         val mastodon: String = "https://mastodon.social/@yasanglass",
         val playStore: String = "https://play.google.com/store/apps/dev?id=5035207490031558874",
-        val discordServer: String = "https://discord.gg/8BQrfyA",
+        val telegramChannel: String = "https://t.me/YASANupdates",
+        val website: String = "https://yasan.glass",
     )
 
 }
