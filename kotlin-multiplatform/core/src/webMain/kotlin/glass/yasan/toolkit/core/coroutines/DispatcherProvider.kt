5f3078f9da18b4ec1ce07d40c2443aa4d13f9b38
Yasan Glass <yasanglass@gmail.com>
2025-10-29 00:40:17 +0100

    [skip ci] Fix wrong comment

--- kotlin-multiplatform/core/src/webMain/kotlin/glass/yasan/toolkit/core/coroutines/DispatcherProvider.kt
+++ kotlin-multiplatform/core/src/webMain/kotlin/glass/yasan/toolkit/core/coroutines/DispatcherProvider.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.Dispatchers
 
 public actual class DefaultDispatcherProvider : DispatcherProvider {
     actual override val main: CoroutineDispatcher = Dispatchers.Main
-    actual override val io: CoroutineDispatcher = Dispatchers.Main // Wasm doesn't have IO
+    actual override val io: CoroutineDispatcher = Dispatchers.Main // Web doesn't have IO
     actual override val default: CoroutineDispatcher = Dispatchers.Default
     actual override val unconfined: CoroutineDispatcher = Dispatchers.Unconfined
 }
