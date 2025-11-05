191523c7a8335824a7179793a657c58e008ae5e2
Yasan Glass <yasanglass@gmail.com>
2025-11-05 22:33:23 +0100

    Create collectAsStateWithLifecycleIfAvailable

---
+++ kotlin-multiplatform/compose/src/commonMain/kotlin/glass/yasan/toolkit/compose/coroutines/FlowExtensions.kt
@@ -0,0 +1,14 @@
+package glass.yasan.toolkit.compose.coroutines
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+
+@Composable
+public expect fun <T> Flow<T>.collectAsStateWithLifecycleIfAvailable(
+    initialValue: T,
+): State<T>
+
+@Composable
+public expect fun <T> StateFlow<T>.collectAsStateWithLifecycleIfAvailable(): State<T>
