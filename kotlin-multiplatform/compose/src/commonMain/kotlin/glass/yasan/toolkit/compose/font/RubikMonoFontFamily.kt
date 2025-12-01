d9fedfa6f399aa9698fe908788f3e4ef006a771d
Yasan Glass <yasanglass@gmail.com>
2025-12-01 20:29:00 +0100

    Fix wrong font being used for Rubik Mono font family composable

--- kotlin-multiplatform/compose/src/commonMain/kotlin/glass/yasan/toolkit/compose/font/RubikMonoFontFamily.kt
+++ kotlin-multiplatform/compose/src/commonMain/kotlin/glass/yasan/toolkit/compose/font/RubikMonoFontFamily.kt
@@ -2,13 +2,11 @@ package glass.yasan.toolkit.compose.font
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import glass.yasan.toolkit.compose.Res
-import glass.yasan.toolkit.compose.rubik_black
+import glass.yasan.toolkit.compose.rubik_mono
 import org.jetbrains.compose.resources.Font
 
 @Composable
 public fun rubikMonoFontFamily(): FontFamily = FontFamily(
-    Font(Res.font.rubik_black, FontWeight.Black, FontStyle.Normal),
+    Font(Res.font.rubik_mono),
 )
