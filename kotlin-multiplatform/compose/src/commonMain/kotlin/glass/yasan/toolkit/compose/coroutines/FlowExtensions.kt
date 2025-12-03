3f81fa1504b4c9efa8d9752df73486158fc70ced
Yasan Glass <yasanglass@gmail.com>
2025-12-03 07:51:21 +0100

    Add documentation for `collectAsStateWithLifecycleIfAvailable`

--- kotlin-multiplatform/compose/src/commonMain/kotlin/glass/yasan/toolkit/compose/coroutines/FlowExtensions.kt
+++ kotlin-multiplatform/compose/src/commonMain/kotlin/glass/yasan/toolkit/compose/coroutines/FlowExtensions.kt
@@ -5,10 +5,16 @@ import androidx.compose.runtime.State
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
+/**
+ * Internally either uses `collectAsStateWithLifecycle` or `collectAsState()` depending on the target platform.
+ */
 @Composable
 public expect fun <T> Flow<T>.collectAsStateWithLifecycleIfAvailable(
     initialValue: T,
 ): State<T>
 
+/**
+ * Internally either uses `collectAsStateWithLifecycle` or `collectAsState()` depending on the target platform.
+ */
 @Composable
 public expect fun <T> StateFlow<T>.collectAsStateWithLifecycleIfAvailable(): State<T>
